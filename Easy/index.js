b388ca7fd8bc40b819e2f98497d4ef6a2180fc11
Duongwphan <duongp2607@gmail.com>
2021-06-10 11:02:45 +0700

    Excercises-7 total odd number in an  array

--- Easy/index.js
+++ Easy/index.js
@@ -1,13 +1,11 @@
-
-   // id 6 viết hàm tính tổng số chẵn trong một mảng
-   var  tall =[9,8,7,6,5,4,3,2,1];
-   total( tall);
-   function total(arr){
-     var  totalEven = 0;
-     for (var i = 0; i< arr.length; i++){
-       if (arr[i] % 2  === 0){
-         totalEven = totalEven + tall[i];
-       }
-     }
-     console.log( "totalEven have :" + totalEven);
-   }
+ var tall=[2,5,4,6,1,7,8,];
+ total(tall);
+ function total(arr){
+     var  totalOdd = 0;
+    for (var i = 0; i< arr.length; i++){
+      if (arr[i] % 2  !== 0){
+        totalOdd = totalOdd + tall[i];
+      }
+    }
+    console.log( "totalEven have :" + totalOdd);
+ }
