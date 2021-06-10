414128b5777b8eaf5ad6995769eb529a2b5b37a4
Duongwphan <duongp2607@gmail.com>
2021-06-10 10:53:22 +0700

    Excercises-6 total even number in an array

--- Easy/index.js
+++ Easy/index.js
@@ -1,13 +1,13 @@
 
-//id 5 Viết hàm đếm số lượng lẻ trong một mảng  
-   var tall =[1,2,3,4,5,6,7,8,9,10,11,12];
-   duolingo(tall);
-   function duolingo(arr){
-      var sumOdd =0;
-      for (var i = 0; i < arr.length; i++){
-        if (arr[i] % 2 !==0){
-          sumOdd++;
-        }
-      }
-      console.log(" sumOdd number have :" +sumOdd);
+   // id 6 viết hàm tính tổng số chẵn trong một mảng
+   var  tall =[9,8,7,6,5,4,3,2,1];
+   total( tall);
+   function total(arr){
+     var  totalEven = 0;
+     for (var i = 0; i< arr.length; i++){
+       if (arr[i] % 2  === 0){
+         totalEven = totalEven + tall[i];
+       }
+     }
+     console.log( "totalEven have :" + totalEven);
    }
