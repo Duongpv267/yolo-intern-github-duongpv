6f23c83bcc6ed7fe1ed454c889d91e8c114fe20e
Duongwphan <duongp2607@gmail.com>
2021-06-11 12:11:12 +0700

    Excercises-13 write reverse order function

--- Easy/index.js
+++ Easy/index.js
@@ -1,11 +1,6 @@
-// id 12 Viết hàm tìm giá trị trung bình của một mảng các số
- function average(a, n){
-   var sum =0 ;
-   for ( var i  = 0; i < n; i++ ){
-     sum += a[i];
-   }return parseFloat(sum/n);
- }
- var arr = [1,2,3,4,5,6,7,8,9];
- var n = arr.length;
- console.log(average(arr ,n));
- console.log("<br>");
+// id 13 Viết hàm thứ tự  đảo ngược
+const array =[1,2,3,4,5,6,7,8,9];
+console.log('array:', array);
+const reversed = array.reverse();
+console.log('reversed:' ,reversed);
+
