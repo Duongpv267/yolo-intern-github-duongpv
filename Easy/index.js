a99276a57a31d1264fb713ad76f5dab5dcc22852
Duongwphan <duongp2607@gmail.com>
2021-06-11 09:33:10 +0700

    Excercise 10,11 find the maxximum or minximum value in an array of numbers

--- Easy/index.js
+++ Easy/index.js
@@ -1,20 +1,4 @@
-function remainder(a, b) {
-  if (a === 0) {
-    return 0;
-  }
-  if (b === 0 || isNaN(a) || isNaN(b)) {
-    return NaN;
-  }
-  if (a < b) {
-    return a;
-  }
-  if (a > 0 && b > 0) {
-    var counter = a;
-    while (counter >= Math.abs(b)) {
-      counter = counter - b;
-    }
-    return counter;
-  }
-}
-var output =remainder(10,3);
-console.log(output);
+// id 10 Viết hàm tìm giá trị lớn nhất trong 1 mảng các số
+console.log(Math.max (1,3,2,4,5,6,7,3,5,9,-1,-5,-6));
+// id 11 Viết hàm tìm giá trị nhỏ nhất trong 1 mảng các số
+console.log(Math.min (1,3,4,2,6,7,4,8,9,4,3));
