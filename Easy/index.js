c419347ef8d87df7e1a542c9161286e402783e10
Duongwphan <duongp2607@gmail.com>
2021-06-12 10:51:38 +0700

    Excercises-14 factorial of number

--- Easy/index.js
+++ Easy/index.js
@@ -1,11 +1,5 @@
-// id 14 Viết hàm tìm vị trí của một sô trong một mảng các số
-// sử dụng phương thức indexOf() phương thức này trả về chỉ số cảu lần xuất hiện 
-//đầu tiên phần tử bạn muốn tìm hoặc -1 nếu phần tử không được tìm thấy.
-// sử dụng thuật toán so sánh như toán tử(===)
-// ---Array.indexOf (seachElement, fromIndex) <từ trái qua phải>
-//---lastindexOf (seachElement, fromIndex= aray.lenght -1) <từ phải qua trái>
- var scores =[1,2,3,4,5,6,7];
- console.log("number of things" + scores.indexOf(4));
- console.log("number of things" + scores.indexOf(9));
- console.log("number of things" + scores.lastIndexOf(5));
- console.log("number of things" + scores.lastIndexOf(9));
+ function factorial (n){
+     return (n == 0 || n==0 ) ? 1:
+     n * factorial(n - 1);
+ }
+ alert("factorial is"+ factorial(5));
