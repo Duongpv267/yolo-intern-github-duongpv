17ad5b76eb5f4d77489e73563d9c784e770dd566
Duongwphan <duongp2607@gmail.com>
2021-06-12 13:55:42 +0700

    Exercises -16,17,18,19,20 True or false

--- Easy/index.js
+++ Easy/index.js
@@ -1,5 +1,15 @@
- function factorial (n){
-     return (n == 0 || n==0 ) ? 1:
-     n * factorial(n - 1);
- }
- alert("factorial is"+ factorial(5));
+
+// id 16 Chuỗi logic sau trả về true hay false
+// ((1<3) && (2<4) || (11<25 || 16> 21)) && true
+alert(true)
+// id 17 Chuỗi logic sau trả về true hay false
+// ((5<6) || (4<2)) && (21<20 && 13 < 11) && false
+
+// id 18 Chuỗi logic sau trả về true hay false
+// ((true && false) || (false && false)) && (false || true)
+
+// id 19 Chuỗi logic sau trả về true hay false
+// ((true || false) && (true || false)) || (true && false)
+
+// id 20 Chuỗi logic sau trả về true hay false ( ! là ngược lại ví dụ !true = false và !false = true)
+// !(!(true || false) && (!true && false)) && (true && !false)
