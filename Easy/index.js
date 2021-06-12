f013cbbe051b257cf0b6ce6cc063263b61f7a84a
Duongwphan <duongp2607@gmail.com>
2021-06-12 15:15:25 +0700

    Excercises-21,22,23 difference, debug, understand code

--- Easy/index.js
+++ Easy/index.js
@@ -1,15 +1,24 @@
+// id 21 sự khác nhau giữa 
+// i++ và ++i (i là một số nguyên)
+++i tăng giá trị của i lên 1 và trả về giá trị mới đó 
+i++ cũng tăng giá trị i lên 1 nhưng trả về giá trị ban đầu của i trước khi được tăng lên 1
 
-// id 16 Chuỗi logic sau trả về true hay false
-// ((1<3) && (2<4) || (11<25 || 16> 21)) && true
-alert(true)
-// id 17 Chuỗi logic sau trả về true hay false
-// ((5<6) || (4<2)) && (21<20 && 13 < 11) && false
+// id 22 Đoạn code sau có vấn đề gì
+// for(var i=0; i<10; i--) { 
+//     print(i); 
+// }
+sửa :print -> printf ( đây là trong lập trình C)
+sửa: i-- thành i++ 
+hoặc sửa : i<10 thành i> -10
 
-// id 18 Chuỗi logic sau trả về true hay false
-// ((true && false) || (false && false)) && (false || true)
-
-// id 19 Chuỗi logic sau trả về true hay false
-// ((true || false) && (true || false)) || (true && false)
-
-// id 20 Chuỗi logic sau trả về true hay false ( ! là ngược lại ví dụ !true = false và !false = true)
-// !(!(true || false) && (!true && false)) && (true && !false)
+// id 23 Đoạn code sau trả về giá trị bao nhiêu 
+// (% là toán tử lấy số dư, ví dụ 3%2 = 1)
+// int total = 0;
+// for(int i=0; i<11; i++) {
+//    if(i %2 == 0) {
+//        total += 1;
+//    } else {
+//        total += 2;
+//    }
+// }
+// return total;
