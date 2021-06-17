c74c26c3d751fb8af33f4fe8e8d21420b09113e1
Duongwphan <duongp2607@gmail.com>
2021-06-17 13:19:24 +0700

    Excercises-4 function to subtract the values of arrays

--- Medium/index.js
+++ Medium/index.js
@@ -1,7 +1,8 @@
 //id 2 viết 2 mảng 
-let a=[1,2,3,4,5,6];
+let a=[12,11,13,23,5,9];
 let b=[6,7,8,9,3,4];
-var sum= a.map(function(num,idx){
-  return num + b[idx];
-})
-alert("Array c :"+ sum);
+var c =[];
+for (let i=0; i< a.length;i++){
+  c.push(Math.abs(a[i]-b[i]));
+}
+alert("Array c: "+c);
