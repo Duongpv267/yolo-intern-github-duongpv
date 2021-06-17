41bf11cd69fd871134fc46f9820f45360ea1b0ba
Duongwphan <duongp2607@gmail.com>
2021-06-17 13:37:13 +0700

    Excercises-6 function to divide the value of 2 arrays

--- Medium/index.js
+++ Medium/index.js
@@ -1,8 +1,8 @@
-//id 2 viết 2 mảng 
+//id 6 chia 2 mảng số
 let a=[12,11,13,23,5,9];
 let b=[6,7,8,9,3,4];
 var c =[];
 for (let i=0; i< a.length;i++){
-  c.push(Math.abs(a[i]*b[i]));
+  c.push(Math.abs(a[i]/b[i]));
 }
 alert("Array c: "+c);
