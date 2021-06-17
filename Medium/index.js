04a03aa58773558d387ad75bfef185bef7d88820
Duongwphan <duongp2607@gmail.com>
2021-06-17 11:39:17 +0700

    Excercises-3 function that sum each value os an array

--- Medium/index.js
+++ Medium/index.js
@@ -1,4 +1,7 @@
 //id 2 viết 2 mảng 
 let a=[1,2,3,4,5,6];
-let b=[6,7,8,9];
-alert("Array c :"+a.concat(b));
+let b=[6,7,8,9,3,4];
+var sum= a.map(function(num,idx){
+  return num + b[idx];
+})
+alert("Array c :"+ sum);
