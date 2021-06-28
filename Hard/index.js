35eb5840211aac3b466884d101d7b3af2530e10b
Duongwphan <duongp2607@gmail.com>
2021-06-28 10:58:04 +0700

    Excercises -7 write function for the number of string in string

--- Hard/index.js
+++ Hard/index.js
@@ -1,13 +1,22 @@
-function xaoTron(mang){
-    var bien = mang.length, ngauNhien;
-    while(0 != bien){
-        ngauNhien = Math.floor(Math.random() * bien);
-        bien --;
-        [mang[xaoTron], mang[ngauNhien]] 
-        = [ mang[ngauNhien], mang[xaoTron]];
+let mod = 100000007;
+function bienDem(mang1, mang2) {
+    let bien = 0;
+    let a = mang1.length;
+    let b = mang2.length;
+    for (i = 0; i <= b - a; i++) {
+        let c;
+        for (c = 0; c < a; c++) {
+            if (mang2[i + c] != mang1[c]) {
+                break;
+            }
+        }
+        if (c == a) {
+            bien++;
+            c = 0;
+        }
     }
-    return mang;
+    return bien;
 }
-var mang =[23,64,32,36,07,02,34,55,11];
-xaoTron(mang);
-alert('Cac so bi xao tron: '+ mang)
+let mang1 = "phanvanduong";
+let mang2 = "an";
+document.write(bienDem(mang2, mang1));
