0a88821096e4b9c65080078cb06743c5dfbcbbcd
Duongwphan <duongp2607@gmail.com>
2021-07-01 16:15:44 +0700

    edit

--- Medium/index.js
+++ Medium/index.js
@@ -1,17 +1,21 @@
-function chuoi(c2, c1) {
-	var count = 0;
-	for (var i = 0; c1.length; i++) {
-		if (count == c2.length) {
-			break;
-		} if (c2[count] == c1[i]) {
-			count++;
-		} else {
-			if (count > 0) {
-				i -= count;
-			} count = 0;
-		}
-	}
-	return count < c2.length ? -1 : i - count;
+let chuSo = [1, 1, 2, 6, 4, 2, 2, 4, 2, 8];
+       
+function chuSoCuoiCungKhac0(n)
+{
+	if (n < 10)
+		return chuSo[n];
+	if ((parseInt(n / 10, 10) % 10) % 2 == 0)
+	 return (6 * chuSoCuoiCungKhac0(parseInt(n / 5, 10))
+		* chuSo[n % 10]) % 10;
+	else
+	 return (4 * chuSoCuoiCungKhac0(parseInt(n / 5, 10))
+		* chuSo[n % 10]) % 10;
 }
-var c1 = "phanvanduongph18124";
-document.write("vị trí thứ"+chuoi("duong", c1));
+ 
+let n = 14;
+  document.write(chuSoCuoiCungKhac0(n));
+// Kiểm tra xem chữ số hàng chục (hoặc thứ hai 
+// cuối) là lẻ hay chẵn 
+// Nếu n = 375, Vậy n / 10 = 37 và 
+// (n / 10)% 10 = 7 Áp dụng công thức 
+// cho chẵn và lẻ các trường hợp.
